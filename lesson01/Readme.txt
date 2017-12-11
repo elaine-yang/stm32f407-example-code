24c8610c33f2903b2471bcd0dd750263ec4e1101
elaine-yang <elaine_yang@163.com>
2017-12-11 20:33:49 +0800

    Add files via upload

---
+++ lesson01/Readme.txt
@@ -0,0 +1,8 @@
+新建一个项目，选择芯片STM32F407ZG,将startup_stm32f40_41xxx.s加入项目。
+创建USER文件夹，保存用户源代码和项目文件。
+
+1）顺序执行，观测PC寄存器的变化
+2）int类型，最大正数0X7FFFFFFF 最大负数0x80000000 最小负数0xFFFFFFFF
+3）观测APSR寄存器NZCV位的变化。
+
+
---
+++ lesson01/USER/test.c
@@ -0,0 +1,29 @@
+
+int main() {
+
+    int counter = 0;
+
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+
+    return 0;
+}
+
+
+
